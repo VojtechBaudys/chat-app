045a87420b548bb071d0bcd1f2974b2c734de24a
VojtechBaudys <vojta@baudys.name>
2022-03-30 19:24:56 +0200

    velké vymoženosti

--- client.py
+++ client.py
@@ -1,99 +1,118 @@
+from fileinput import close
 import json
 import tkinter as tk
 from tkinter import scrolledtext
 import socket
 import threading
 
-def leave_login(obj, msg):
+def close_app(msg): # close app
+    try:
+        s.send(msg.encode('utf8')) # send disconnect msg to server
+    except:
+        pass
+    root.destroy() # destroy tk window
+
+def leave_login(obj, msg): # login entry mouse leave
 	if obj.get() == '':
 		obj.delete(0, 'end')
 		obj.insert(0, msg)
 
-def send_msg(event):
-	msg = entry_box.get()
-	if msg == '/help':
+def enter_login(obj, msg): #login entry mouse enter
+    if obj.get() == msg:
+        obj.delete(0, 'end')
+
+def send_msg(event): # send msg to server
+	msg = entry_box.get() # get msg from entry
+	if msg == '/help': # help command
 		entry_box.delete(0, tk.END)
+        # write commands to text area
 		text_box.config(state='normal')
-		text_box.insert(tk.END, '\n' + 'COMMANDS' + '\n' + '/users' + '\n' + '/leave')
+		text_box.insert(tk.END, '\n' + 'COMMANDS' + '\n' + '/online [show online users]' + '\n' + '/offline [show offline users]' + '\n' + '/leave [shut down app]' + '\n')
 		text_box.config(state='disabled')
-	elif msg == '/users':
-		s.send(msg.encode('utf8'))
-		entry_box.delete(0, tk.END)
-	elif msg == '/leave':
-		s.send(msg.encode('utf8'))
-		root.destroy()
-	elif msg != '':
-		if len(msg) <= 1000 and msg[0] != '/':
-			s.send(msg.encode('utf8'))
-			entry_box.delete(0, tk.END)
+	elif msg == '/online' or msg == '/offline': # offline command
+		s.send(msg.encode('utf8')) # send msg
+		entry_box.delete(0, tk.END) # clear entry box
+	elif msg == '/leave': # leave command
+		close_app(msg)
+	elif msg != '': # if is not empty
+		if len(msg) <= 1000 and msg[0] != '/': # max len msg
+			s.send(msg.encode('utf8')) # send normal msg to server
+			entry_box.delete(0, tk.END) # clear entry box
 
-def msg_lisener():
-	while True:
-		msg = s.recv(1000)
-		msg = msg.decode('utf8')
-		print(msg)
-		text_box.config(state='normal')
-		text_box.insert(tk.END, msg + '\n')
-		text_box.config(state='disabled')
+def msg_lisener(): # listen server msg when logged
+    while True:
+        try:
+            msg = s.recv(1000) # get msg
+        except:
+            break
+        msg = msg.decode('utf8') # decode msg
+        # write msg to text area
+        text_box.config(state='normal')
+        text_box.insert(tk.END, msg + '\n')
+        text_box.config(state='disabled')
 
-def listen_login():
-	while True:
-		msg = s.recv(1000)
-		msg = msg.decode('utf8')
-		print(msg)
-		if msg == '/success':
-			login_email.destroy()
-			login_password.destroy()
-			login_button.destroy()
-			login_label.destroy()
-			
-			global text_box
-			global entry_box
-			global button1
+def listen_login(): # listen login status
+    while True:
+        try:
+            msg = s.recv(1000) # get loggin status
+        except:
+            break
+        msg = msg.decode('utf8') # decode msg
+        if msg == '/success': # login success
+            # destroy login form
+            login_email.destroy()
+            login_password.destroy()
+            login_button.destroy()
+            login_label.destroy()
 
-			text_box = scrolledtext.ScrolledText(
-				width = 40,
-				height = 40,
-				wrap="word"
-			)
-			text_box.pack()
-			text_box.insert(tk.END, 'Welcome to the Tinder...' + '\n')
-			text_box.config(
-				state='disabled',
-			)
+            global text_box
+            global entry_box
+            global button1
 
-			entry_box = tk.Entry(
-				width=30
-			)
-			entry_box.pack()
+            # setup chat
+            text_box = scrolledtext.ScrolledText(
+                width = 40,
+                height = 40,
+                wrap="word"
+            )
+            text_box.pack()
+            text_box.insert(tk.END, 'Welcome to the Badoo...' + '\n' + 'HELP COMMAND: /help' + '\n')
+            text_box.config(
+                state='disabled',
+            )
 
-			button1 = tk.Button(
-				text='odeslat',
-				width=20,
-				bg='pink',
-				command=lambda event='': send_msg(event)
-			)
-			button1.pack()
-			
-			threading.Thread(target = msg_lisener).start()
-			root.bind('<Return>', lambda event: send_msg(event))
+            entry_box = tk.Entry(
+                width=30
+            )
+            entry_box.pack()
 
-			break
+            button1 = tk.Button(
+                text='odeslat',
+                width=20,
+                bg='pink',
+                command=lambda event='': send_msg(event)
+            )
+            button1.pack()
 
-def send_login():
-	email = login_email.get()
-	password = login_password.get()
-	if email != '' and email != 'Email' and password != '' and password != 'Password':
+            threading.Thread(target = msg_lisener).start() # start listening msg
+            root.bind('<Return>', lambda event: send_msg(event))
+
+            break
+
+def send_login(): # send login json (email, passowrd)
+	email = login_email.get() # get email from entry
+	password = login_password.get() # get password from entry
+	if email != '' and email != 'Email' and password != '' and password != 'Password': # if is not null
+        # create json 
 		data = {
 			'email': email,
 			'password': password
 		}
 		data = json.dumps(data)
 		if len(data) <= 1000:
-			s.send(data.encode('utf8'))
-	print('login')
+			s.send(data.encode('utf8')) # send json
 
-# ---- Connect ----
+# connect to server
 s = socket.socket(
 	socket.AF_INET,
 	socket.SOCK_STREAM
@@ -103,9 +122,10 @@ host = '127.0.0.1'
 port = 2205
 s.connect((host, port))
 
-# ---- Tkinter ----
+# login form setup
 root = tk.Tk()
-root.title('Tinder')
+root.title('Badoo')
+root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='badoo.png'))
 
 login_label = tk.Label(
 	text='LOGIN'
@@ -117,7 +137,7 @@ login_email = tk.Entry(
 )
 login_email.pack()
 login_email.insert(0, 'Email')
-login_email.bind('<Button-1>', lambda event='': login_email.delete(0, 'end'))
+login_email.bind('<Button-1>', lambda event='': enter_login(login_email, 'Email'))
 login_email.bind('<FocusOut>', lambda event='': leave_login(login_email, 'Email'))
 
 login_password = tk.Entry(
@@ -125,7 +145,7 @@ login_password = tk.Entry(
 )
 login_password.pack()
 login_password.insert(0, 'Password')
-login_password.bind('<Button-1>', lambda event='': login_password.delete(0, 'end'))
+login_password.bind('<Button-1>', lambda event='': enter_login(login_password, 'Password'))
 login_password.bind('<FocusOut>', lambda event='': leave_login(login_password, 'Password'))
 
 login_button = tk.Button(
@@ -135,6 +155,7 @@ login_button = tk.Button(
 login_button.pack()
 
 root.bind('<Return>', lambda event: send_login())
+root.protocol("WM_DELETE_WINDOW", lambda event='': close_app('/leave'))
 
 threading.Thread(target=listen_login).start()
 
--- server.py
+++ server.py
@@ -1,66 +1,97 @@
 import json
 import socket
 import threading
+import time
+import logging
 
-def listen():
+# logging config
+logging.basicConfig(level=logging.DEBUG, filename='server.log', filemode='w', format='[%(asctime)s] [%(levelname)s]: %(message)s')
+
+# functions
+def listen(): # listen new clients
+    id = 0
     while True:
         serversocket.listen()
-        clientsocket = serversocket.accept()[0]
-        print(clientsocket)
-        clients.append({'id': len(clients) ,'logged': False, 'socket': clientsocket, 'username': ''})
-        threading.Thread(target=user_handler, args=(clients[-1],)).start()
+        clientsocket, addr = serversocket.accept()
+        logging.info(addr[0] + ' connected to server') # log client connected
+        clients.append({'id': id ,'logged': False, 'socket': clientsocket, 'addr': addr[0], 'username': ''}) # client vars
+        id += 1
+        threading.Thread(target=user_handler, args=(clients[-1],)).start() # client thread
         
-def user_handler(client):
-    print(client)
+def user_handler(client): # client handler
     while client:
-        if client['logged']:
-            msg = client['socket'].recv(1000).decode('utf8')
-            if msg:
-                print(msg)
-                if msg == '/users':
-                    msg = '\n\nUSERS\n'
+        if client['logged']: # is logged
+            try:
+                msg = client['socket'].recv(1000).decode('utf8') # get client msg
+            except:
+                break
+            if msg: # entry msg
+                if msg == '/online': # online clients commands
+                    msg = '\nONLINE USERS\n'
                     for one_client in clients:
                         msg += one_client['username'] + '\n'
-                    print(client)
-                    client_msg(client['socket'], msg)
-                elif msg == '/leave':
-                    clients.pop(client['id'])
-                    client = False
-                    print(clients)
-                    print(client)
+                    client_msg(client['socket'], msg) # send online users
+                elif msg == '/offline': # offline clients command
+                    msg = '\nOFFLINE USERS\n'
+                    with open('user_data.json', 'r') as us:
+                        users = json.load(us)
+                    for user_db in users:
+                        offline = True
+                        for user_list in clients:
+                            if users[user_db]['name'] == user_list['username'] and user_list['logged']: # if user is logged/connected
+                                offline = False
+                                break
+                        if offline:
+                            msg += users[user_db]['name'] + '\n'
+                    client_msg(client['socket'], msg) # send offline users
+                elif msg == '/leave': # disconnect command
+                    remove_client(client['id'], client['addr']) # remove user from connected clients
                 else:
-                    send_msg(client['username'], msg)
-        else:
-            res = client['socket'].recv(1000).decode('utf8')
-            print(res)
-            res = json.loads(res)
-            with open('user_data.json', 'r') as us:
+                    logging.info(client['username'] + ' send message: "' + msg + '"') # log user msg
+                    send_msg(client['username'], msg) # send msg to all logged users
+        else: # is not logged
+            try:
+                res = client['socket'].recv(1000).decode('utf8') # get client email and password / disconnect command
+            except:
+                break
+            if res == '/leave': # disconnect client
+                remove_client(client['id'], client['addr']) # remove user from connected clients
+                break
+            res = json.loads(res) # load user email and password
+            with open('user_data.json', 'r') as us: # load user data 
                 user_data = json.load(us)
             try:
-                if user_data[res['email']]:
-                    if user_data[res['email']]['password'] == res['password']:
-                        clients[client['id']]['logged'] = True
-                        clients[client['id']]['username'] = user_data[res['email']]['name']
-                        client_msg(client['socket'], '/success')
-                        print(client)
+                if user_data[res['email']]: # if email exist
+                    if user_data[res['email']]['password'] == res['password']: # if password is correct
+                        clients[client['id']]['logged'] = True # change to logged True
+                        clients[client['id']]['username'] = user_data[res['email']]['name'] # set client username
+                        client_msg(client['socket'], '/success') # send success msg
+                        logging.info(client['username'] + ' logged to Badoo') # log logged client
                     else:
-                        client_msg(client['socket'], '/error')
+                        client_msg(client['socket'], '/error') # send error msg
                 else: 
-                    client_msg(client['socket'], '/error')
+                    client_msg(client['socket'], '/error') # send error msg
             except:
-                client_msg(client['socket'], '/error')
+                client_msg(client['socket'], '/error') # send error msg
 
-def client_msg(client, msg):
+def client_msg(client, msg): # send msg to one client
     msg = msg.encode('utf8')
     client.send(msg)
 
-def send_msg(username, msg):
-    msg = (username + ': ' + msg).encode('utf8')
+def send_msg(username, msg): # send msg to all clients
+    msg = (time.strftime('%m/%d/%Y', time.localtime()) + ' ' + username + ': ' + msg).encode('utf8')
     for client in clients:
         client['socket'].send(msg)
 
+def remove_client(id, addr): # disconnect client
+    clients.pop(id)
+    logging.info(addr + ' disconnected from server') # log disconnected client
+    client = False
+    
 clients = []
-serversocket = socket.socket(
+
+# Server
+serversocket = socket.socket( 
     socket.AF_INET,
     socket.SOCK_STREAM
 )
@@ -70,10 +101,13 @@ port = 2205
 try:
     serversocket.bind((host, port))
 
-    lis = threading.Thread(target=listen)
+    lis = threading.Thread(target=listen) # main listen thread
     lis.start()
-    lis.join()
+except Exception as e: # main start error
+    logging.error(str(e) + ' (Line: ' + str(e.__traceback__.tb_lineno) + ')') # log server start error
+else:
+    logging.info('server started') # log server started
 
-    serversocket.close()
-except:
-    print('error')
+lis.join()
+
+serversocket.close() # server socket close
